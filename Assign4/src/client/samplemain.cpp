e8b7135519636d634048aa4de7359bc8bab954a6
GeneHFang <gene.li@nyu.edu>
2020-04-15 00:58:40 -0400

    new test

--- Assign4/src/client/samplemain.cpp
+++ Assign4/src/client/samplemain.cpp
@@ -360,10 +360,10 @@ public:
          for (int j = 0 ; j < md.episodes.size() ; j++){
             string epTitle =  md.episodes[i].title;
             string add = root+"/"+epTitle; 
-            tree->add(add);
+            tree->add(add.c_str);
          }
          string close = "/"+root;
-         tree->close(close);
+         tree->close(close.c_str);
          cout << md.title << " " << md.titleAndSeason << " " << md.rating
               << " " << md.genre << endl;
       }
