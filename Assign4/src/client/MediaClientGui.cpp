3fcd30e8a2f983a42d76bddd642a4c42207e8bc7
GeneHFang <gene.li@nyu.edu>
2020-04-15 04:54:36 -0400

    fixes

--- Assign4/src/client/MediaClientGui.cpp
+++ Assign4/src/client/MediaClientGui.cpp
@@ -139,9 +139,9 @@ public:
       // create a tree control at position x=10, y=10. Its 150 pixels wide
       // and window height less 20 pixels high. Add some sample tree nodes.
       tree = new Fl_Tree(10, 120, 325, this->h()-135);
-      // tree->add("Flintstones/Fred");
-      // tree->add("Flintstones/Wilma");
-      // tree->close("/Flintstones");
+      tree->add("Flintstones/Fred");
+      tree->add("Flintstones/Wilma");
+      tree->close("/Flintstones");
 
       /*
        * add a text input control at x=250, y=35 of width 200 pixels and
