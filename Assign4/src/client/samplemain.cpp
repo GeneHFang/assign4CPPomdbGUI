63e9a62483e81c4894a3ea2d37075c613524496c
GeneHFang <gene.li@nyu.edu>
2020-04-15 02:16:26 -0400

    new test

--- Assign4/src/client/samplemain.cpp
+++ Assign4/src/client/samplemain.cpp
@@ -387,7 +387,6 @@ public:
       cout << endl;
       Fl_JPEG_Image * img = new Fl_JPEG_Image("temp.jpg");
       delete png;
-      box->clear();
       box->image(img);
       box->redraw();
       box->show();
